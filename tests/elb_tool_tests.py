b7e33ed45659affd47fc9273d5fdf80b3573de9a
Rob Terhaar <rob@atlanticdynamic.com>
2011-09-16 15:04:40 -0400

    added a 2nd test for debug output

--- tests/elb_tool_tests.py
+++ tests/elb_tool_tests.py
@@ -5,4 +5,10 @@ def debug_test():
     ui = UserInput(['-e', 'elb', '-i', 'inst', '--debug'])
     assert_equal(ui.debug_enabled(), True)
 
+def debug_test2():
+    ui = UserInput(['-e', 'elb', '-i', 'inst', '--debug'])
+    data = ui.get_args_as_dict()
+    return_dict = {'instance_name': 'inst', 'debug': True, 'elb_name': 'elb', 'noop': False}
+    assert_equal(data, return_dict)
+    
 
