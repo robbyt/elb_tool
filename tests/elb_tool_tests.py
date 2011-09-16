67a5e3c03f365b9488328871827db07ada22b9b8
Rob Terhaar <rob@atlanticdynamic.com>
2011-09-16 15:21:26 -0400

    split out UserInput to different module, added a test for new noop method

--- tests/elb_tool_tests.py
+++ tests/elb_tool_tests.py
@@ -11,4 +11,9 @@ def debug_test2():
     return_dict = {'instance_name': 'inst', 'debug': True, 'elb_name': 'elb', 'noop': False}
     assert_equal(data, return_dict)
     
-
+def noop_test():
+    ui = UserInput(['-e', 'elb', '-i', 'inst', '--debug', '--noop'])
+    data = ui.get_args_as_dict()
+    return_dict = {'instance_name': 'inst', 'debug': True, 'elb_name': 'elb', 'noop': True}
+    assert_equal(data, return_dict)
+    
