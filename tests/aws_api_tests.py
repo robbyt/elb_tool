55e98152ebcea1a160efe79bd0ae209d80ba8b0d
Rob Terhaar <rob@atlanticdynamic.com>
2011-09-16 17:34:48 -0400

    better names for tests

--- tests/aws_api_tests.py
+++ tests/aws_api_tests.py
@@ -2,15 +2,15 @@ from nose.tools import *
 from elb_tool.aws_api import *
 import os
 
-def env_test():
+def env_test_key():
     conn = ElbConnection()
     assert_equal(conn.aws_key, os.environ.get('EC2_ACCESS_KEY'))
 
-def env_test2():
+def env_test_sec():
     conn = ElbConnection()
     assert_equal(conn.aws_secret, os.environ.get('EC2_SECRET_KEY'))
 
-def env_test3():
+def env_test_empty_env():
     with assert_raises(EnvError):
         conn = ElbConnection('','')
     
