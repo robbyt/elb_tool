08a7e933c75c22293f1521bb4598f253b12f1845
Rob Terhaar <rob@atlanticdynamic.com>
2011-09-16 15:05:15 -0400

    added some dep modules

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'url': '.',
     'download_url': '.',
     'version': '0.1',
-    'install_requires': ['nose'],
+    'install_requires': ['nose','boto','argparse'],
     'packages': ['elb_tool'],
     'scripts': [],
     'name': 'elb_tool'
