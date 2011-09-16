58bc86449c9b91c9d9de64228897efcbc7bea543
Rob Terhaar <rob@atlanticdynamic.com>
2011-09-16 17:25:57 -0400

    tests for env var read

--- tests/aws_api_tests.py
+++ tests/aws_api_tests.py
@@ -1,7 +1,12 @@
 from nose.tools import *
-from elb_tool.aws_api import *
+import elb_tool.aws_api
 import os
 
-def connection_test():
-    pass
+def env_test():
+    conn = elb_tool.aws_api.ElbConnection()
+    assert_equal(conn.aws_key, os.environ.get('EC2_ACCESS_KEY'))
+
+def env_test2():
+    conn = elb_tool.aws_api.ElbConnection()
+    assert_equal(conn.aws_secret, os.environ.get('EC2_SECRET_KEY'))
     
