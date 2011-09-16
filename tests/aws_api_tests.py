38ee39b1e59244fb23611af0b1027e4fd1f4096a
Rob Terhaar <rob@atlanticdynamic.com>
2011-09-16 17:31:45 -0400

    add test for empty env

--- tests/aws_api_tests.py
+++ tests/aws_api_tests.py
@@ -1,12 +1,16 @@
 from nose.tools import *
-import elb_tool.aws_api
+from elb_tool.aws_api import *
 import os
 
 def env_test():
-    conn = elb_tool.aws_api.ElbConnection()
+    conn = ElbConnection()
     assert_equal(conn.aws_key, os.environ.get('EC2_ACCESS_KEY'))
 
 def env_test2():
-    conn = elb_tool.aws_api.ElbConnection()
+    conn = ElbConnection()
     assert_equal(conn.aws_secret, os.environ.get('EC2_SECRET_KEY'))
+
+def env_test3():
+    with assert_raises(EnvError):
+        conn = ElbConnection('','')
     
