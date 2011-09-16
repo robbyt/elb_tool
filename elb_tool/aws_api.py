2ef828cc71c10b5af8816e54b0a07f2bfa0ac05e
Rob Terhaar <rob@atlanticdynamic.com>
2011-09-16 16:19:02 -0400

    removed need for external env_var class

--- elb_tool/aws_api.py
+++ elb_tool/aws_api.py
@@ -1,9 +1,19 @@
-import boto
+import boto, os
+
+class EnvError(Exception):
+    pass
 
 class ElbConnection(object):
-    def __init__(self, aws_key, aws_secret):
-       self.aws_key = aws_key
-       self.aws_secret = aws_secret
+    def __init__(
+        self,
+        aws_key = os.environ.get('EC2_ACCESS_KEY'),
+        aws_secret = os.environ.get('EC2_SECRET_KEY')
+    ):
+        if aws_key and aws_secret:
+            self.aws_key = aws_key
+            self.aws_secret = aws_secret
+        else:
+            raise EnvError('EC2_ACCESS_KEY and EC2_SECRET_KEY environment variables are not set.')
 
     def connect(self):
         return boto.connect_elb(self.aws_key, self.aws_secret)
@@ -11,5 +21,4 @@ class ElbConnection(object):
     def get_elb_list(self):
         self.conn = self.connect()
         return self.conn.get_all_load_balancers()
-
 
