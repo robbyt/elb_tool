4483fe0fc34b4bc8ab1189a14c9badec73375669
Rob Terhaar <rob@atlanticdynamic.com>
2011-09-16 15:50:23 -0400

    fix variable scoping

--- elb_tool/aws_api.py
+++ elb_tool/aws_api.py
@@ -1,12 +1,12 @@
 import boto
 
 class ElbConnection(object):
-    def __init__(self,aws_key,aws_secret):
+    def __init__(self, aws_key, aws_secret):
        self.aws_key = aws_key
        self.aws_secret = aws_secret
 
     def connect(self):
-        return boto.connect_elb(aws_key, aws_secret)
+        return boto.connect_elb(self.aws_key, self.aws_secret)
 
     def get_elb_list(self):
         self.conn = self.connect()
