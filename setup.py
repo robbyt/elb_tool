400d8f057bab0a1ce3b1c12f143d83d907b2e365
Rob Terhaar <rob@atlanticdynamic.com>
2011-10-02 00:45:13 +0000

    fixes to setup.py for packaging

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import setup, find_packages
+
+readme = open('README').read()
+
+long_description = """
+This is a CLI tool for interacting with Amazon's ELB.
+----
+
+%s
+
+----
+For more information, run ``elb_tool -h``.
+""" % (readme)
 
 config = {
     'description': 'elb_tool',
     'author': 'Rob Terhaar',
+    'author_email': 'rterhaar@atlanticdynamic.com',
+    'license': 'BSD',
     'url': '.',
     'download_url': '.',
     'version': '0.1',
