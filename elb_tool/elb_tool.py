3ea1972773194301b3eff18557cf633eb64b0312
Rob Terhaar <rob@atlanticdynamic.com>
2011-10-02 00:56:20 +0000

    fix up scope include issues

--- elb_tool/elb_tool.py
+++ elb_tool/elb_tool.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
-import aws_api, aws_actions, user_input, sys
-
+import sys
+from user_input import UserInput
+from aws_actions import AwsActions 
 
 if __name__ == '__main__':
     # parse the args
-    ui = user_input.UserInput(sys.argv[1:])
+    ui = UserInput(sys.argv[1:])
 
     # collect the parsed args into a dict
     data = ui.get_args_as_dict()
 
     # throw that dict at the ElbTool to do stuff with the AWS api
-    action = aws_actions.AwsActions(**data)
+    action = AwsActions(**data)
     action.run()
